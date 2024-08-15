5725d51eae9003c59a8de2c3414f592d229a14a6
Ahmed Nagy <ahmed.nagy@souhoola.com>
2024-08-15 21:06:11 +0300

    Initial Commit

---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Ahmed Nagy on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
